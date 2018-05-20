0f8cadfe0e707c96941309b61dfe1667bb7ce42e
ishanjain <ishanjain1991@gmail.com>
2018-05-20 13:39:13 +0530

    render Home page

--- main.go
+++ main.go
@@ -3,17 +3,43 @@ package main
 import (
 	"net/http"
 	"strings"
+	"time"
+	"html/template"
+	"log"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request)  {
+type PageVariables struct {
+	Date string
+	Time string
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello " + message
 	w.Write([]byte(message))
 }
 
-func main()  {
-	http.HandleFunc("/", sayHello)
+func renderHomePage(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	HomePageVars := PageVariables{
+		Date: now.Format("02-01-2006"),
+		Time: now.Format("15:02:05"),
+	}
+
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Fatal("Tempalte parsing error", err)
+	}
+	err = t.Execute(w, HomePageVars)
+	if err != nil {
+		log.Fatal("Could not give back the respone", err)
+	}
+}
+
+func main() {
+	http.Handle("/ftp", http.FileServer(http.Dir("~/workspace")))
+	http.HandleFunc("/", renderHomePage)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
